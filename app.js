5dbe19c54c7a761fe510f1579d0d2ddf25691d5e
Ilya <113635001+kant0rin@users.noreply.github.com>
2022-11-19 23:21:47 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
 const btn = document.querySelector('.button-prikol')
 const place = document.querySelector('.placeholder')
 
+function sendRequest(){
+    const xhr = new XMLHttpRequest()
+    xhr.open('GET', '/getPrikol')
+    xhr.responseType = 'text'
+    xhr.send()
+    xhr.onload = function (){
+        setPrikol(xhr.response);
+
+    }
+}
+
 function setPrikol(src){
     place.innerHTML = ''
     const pic = document.createElement('img')
     pic.classList.add('pic')
-    pic.src = src
+    btn.classList.add('fixed')
+    pic.src = src;
     place.append(pic)
 }
 
-btn.addEventListener('click', () => {
-    setPrikol('xuy.jpg')
-    btn.classList.add('fixed')
-})
+btn.addEventListener('click', sendRequest)
